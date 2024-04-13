96af3ee7777a20a8c189965df59b754989e131ca
ismayil.khayredinov@holoplot.com <ismayil.khayredinov@holoplot.com>
2024-04-13 17:17:32 +0200

    Fix seek input defaulting to 00:00:00 on every change

--- web/src/grpc/procedures/setKeepSession.ts
+++ web/src/grpc/procedures/setKeepSession.ts
@@ -1,7 +1,6 @@
 import { sessionSourceClient } from '../sessionSourceClient';
 
 export const setKeepSession = async (args: { sessionId: string }) => {
-  console.log(args);
   await sessionSourceClient.setKeepSession({
     sessionID: args.sessionId,
   });
